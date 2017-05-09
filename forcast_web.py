5fc59077ee4ccda2931877ba57806e298dcd549e
Joy_Zhou <zyfzjsc988@outlook.com>
2017-05-09 22:14:40 +0800

    learning : add url redirection method

--- forcast_web.py
+++ forcast_web.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask,url_for
 
 app = Flask(__name__)
 
@@ -8,6 +8,28 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+# host:5000/hello 打印出hello JOY!
+@app.route('/hello')
+def hello():
+    return 'Hello JOY!'
+
+# 使用变量URL ：<variable_name>作为命名参数进行传递
+@app.route('/user/<username>')
+def show_username(username):
+    return 'UserName is %s' % username
+# <converter:variable_name>作为转换器:转换器有int\float\path
+@app.route('/post/<int:post_id>')
+def show_postID(post_id):
+    return 'Post ID is %d' % post_id
+
+# 可以使用url_for模块构造url
+with app.test_request_context():
+    print(url_for('show_username',username='Joy Zhou'))
+
+
+
+
+
 
 # 模块名：模块名为MAIN则其作为单独应用启动，否则可以作为某可导入模块
 if __name__ == '__main__':
