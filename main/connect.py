7f35046c63cf4bed5fce410b92458f0e5a1b4079
Joy_Zhou <zyfzjsc988@outlook.com>
2017-05-22 14:32:01 +0800

    doing:add form+db demo

---
+++ main/connect.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author:J.Zhou
+
+@contact:zyfzjsc988@outlook.com
+
+@file:connect.py
+
+@time:2017/5/22 14:15
+
+@desc:
+
+"""
+
+from flask_sqlalchemy import SQLAlchemy
